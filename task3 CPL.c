a5fae59df09372e96bec40dd5c2ca2b2a49d10b5
eyadelfar <eyadamen588@gmail.com>
2021-11-06 16:50:48 +0200

    updated

---
+++ task3 CPL.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    float num;
+    printf("Please enter a number:\n");
+    scanf("%f",&num);
+    if (num>=0 && num<=25)
+        printf("Interval [0,25]");
+    else if (num>25 && num<=50)
+        printf("Interval (25,50]");
+    else if (num>50 && num<=75)
+        printf("Interval (50,75]");
+    else if (num>75 && num<=100)
+        printf("Interval (75,100]");
+    else
+        printf("Out of intervals");
+}
